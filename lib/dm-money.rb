e3aa8f535193b35285f21728ad49facdd64afc82
Daniel Parker <dcparker@gmail.com>
2009-03-05 00:40:50 -0500

    Fixing money amount display.

--- lib/dm-money.rb
+++ lib/dm-money.rb
@@ -24,7 +24,7 @@ module MoneyType
     def to_s
       (@value < 0 ? '-' : '') + case
       when @value.abs > 99
-        @value.to_s[0..-3] + '.' + @value.to_s[-2..-1]
+        @value.abs.to_s[0..-3] + '.' + @value.abs.to_s[-2..-1]
       when @value.abs > 0
         '.' + @value.abs.to_s
       else
